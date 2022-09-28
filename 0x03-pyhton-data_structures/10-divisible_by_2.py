06ba5339178bcfe4c108caa5d75208a402fdd146
Unyimeabasi D. Abai <105404372+gardianAngel@users.noreply.github.com>
2022-09-28 12:03:40 +0100

    Delete 0x03-pyhton-data_structures directory

--- 0x03-pyhton-data_structures/10-divisible_by_2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-def divisible_by_2(my_list=[]):
-    new_list = []
-    if my_list:
-        for elm in my_list:
-            new_list.append(False if elm % 2 else True)
-        return new_list
