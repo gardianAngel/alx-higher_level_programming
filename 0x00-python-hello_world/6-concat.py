dc141cd34bc91a6678ce34aaa5bab036eeece1d5
gardianAngel <unyimeabai@gmail.com>
2023-01-03 03:30:53 -0800

    task 6 revised

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 str1 = "Holberton"
-
 str2 = "School"
-
-str1 = str1 + ' ' + str2
-
-print("Welcome to {}!".format(str1))
+str3 = str1 + ' ' + str2
+str1 = str3
+print(f"Welcome to {str1}!")
