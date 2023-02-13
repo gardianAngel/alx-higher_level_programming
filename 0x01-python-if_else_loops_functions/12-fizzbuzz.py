f8a84e3f1616ac1a79330badce159430bf9a4e1d
gardianAngel <unyimeabai@gmail.com>
2023-02-13 22:17:54 +0100

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/python3
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("{} ".format("FizzBuzz"), end="")
+    elif i % 5 == 0:
+        print("{} ".format("Buzz"), end="")
+    elif i % 3 == 0:
+        print("{} ".format("Fizz"), end="")
+    else:
+        print("{} ".format(i), end="")
