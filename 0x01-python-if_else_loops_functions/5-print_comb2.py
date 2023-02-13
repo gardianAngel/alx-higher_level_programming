8006d543bd6db48c73c29a291aed51be012e23d5
gardianAngel <unyimeabai@gmail.com>
2023-02-13 21:55:59 +0100

    task 5

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/python3
+for i in range(0, 10):
+    for k in range(0, 10):
+        if k != 9:
+            print("{:n}{:n}, ".format(i, k), end="")
+        elif k == 9:
+            print("{:n}{:n} ".format(i, k), end="")
