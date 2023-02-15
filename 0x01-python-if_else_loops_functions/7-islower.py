c69cc066c2c74ae253b0c277d3774f4e3743d62d
gardianAngel <unyimeabai@gmail.com>
2023-02-15 05:33:40 +0100

    task 7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 7-islower.py
+
+def islower(need='A'):
+    """check for lower case characters"""
+    for i in range(97, 122):
+        if ord(need) == i:
+            print("{}".format(True))
+        else:
+            print("{}".format(False))
+        break
+
