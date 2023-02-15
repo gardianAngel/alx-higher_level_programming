1aa852f65f50b207a38f8ad7815888dec18d695a
gardianAngel <unyimeabai@gmail.com>
2023-02-15 06:16:11 +0100

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 8-uppercase.py
+
+def uppercase(string):
+    """print a string in uppercase followed by a new line"""
+    for i in string:
+        if ord(i) in range(97, 123):
+            i = chr(ord(i) - 32)
+        print("{}\n".format(i), end="")
+
