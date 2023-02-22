4bded8d04378e9d9854f49eb69d1238e256c6e39
gardianAngel <unyimeabai@gmail.com>
2023-02-22 07:18:24 +0000

    refactoring

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-""" A program the prints numbers from 0 - 99"""
-
 for i in range(0,100):
     if i == 99:
         print("{}".format(i))
     else:
-        print("{:02}, ".format(i), end="")
+        print("{:02}".format(i), end=", ")
 
