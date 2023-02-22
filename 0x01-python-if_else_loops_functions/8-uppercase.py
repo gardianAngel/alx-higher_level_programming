5476d5f0bf5057cff32f87203f490f1d3344c57e
gardianAngel <unyimeabai@gmail.com>
2023-02-22 11:37:49 +0100

    refactoring

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@
 def uppercase(string):
     """print a string in uppercase followed by a new line"""
     for i in string:
-        if i in range(97, 123):
-            j = chr(ord(i) - 32)
-        print("{}\n".format(j), end="")
+        if i in range(97, 122):
+            i = chr(ord(i) - 32)
+        print("{}\n".format(i), end="")
 
