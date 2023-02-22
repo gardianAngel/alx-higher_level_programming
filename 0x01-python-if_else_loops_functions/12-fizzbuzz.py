a0a7911a2dd480c6d9eee518bd7d722dd7069e1d
gardianAngel <unyimeabai@gmail.com>
2023-02-22 13:18:03 +0100

    refactoring

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("{} ".format("FizzBuzz"), end="")
-    elif i % 5 == 0:
-        print("{} ".format("Buzz"), end="")
-    elif i % 3 == 0:
-        print("{} ".format("Fizz"), end="")
-    else:
-        print("{} ".format(i), end="")
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("{} ".format("FizzBuzz"), end="")
+        elif i % 5 == 0:
+            print("{} ".format("Buzz"), end="")
+        elif i % 3 == 0:
+            print("{} ".format("Fizz"), end="")
+        else:
+            print("{} ".format(i), end="")
