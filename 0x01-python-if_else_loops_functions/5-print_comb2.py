e0bb04b9a48c584fc11611923ccd70b324174cc5
gardianAngel <unyimeabai@gmail.com>
2023-02-22 07:02:17 +0000

    refactoring

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for k in range(0, 10):
-        if k != 9:
-            print("{:n}{:n}, ".format(i, k), end="")
-        elif k == 9:
-            print("{:n}{:n} ".format(i, k), end="")
+""" A program the prints numbers from 0 - 99"""
+
+for i in range(0,100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}, ".format(i), end="")
+
